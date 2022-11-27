f504c3e151c3338f9fc292d9715c60f0cda5d7ae
GopherJ <alex_cj96@foxmail.com>
2022-11-27 20:28:57 +0800

    update
    Signed-off-by: GopherJ <alex_cj96@foxmail.com>

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '97d27de9a93a45d8a87c6985ecea15d5',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: "CJ' Blog",
+  domain: 'https://tricky-guan-b68.notion.site/I-love-notion-97d27de9a93a45d8a87c6985ecea15d5',
+  author: 'Cheng JIANG',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '0xJ_C',
+  github: 'GopherJ',
+  linkedin: 'alexcj96',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
