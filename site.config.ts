bb00310a69a4da0e4200c35a29195b2cdc9d1a51
GopherJ <alex_cj96@foxmail.com>
2023-07-23 16:00:53 +0800

    update
    Signed-off-by: GopherJ <alex_cj96@foxmail.com>

--- site.config.ts
+++ site.config.ts
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: "CJ's Blog",
-  domain: 'https://www.notion.so/0x1b3f3fd/CJ-s-Blog-97d27de9a93a45d8a87c6985ecea15d5',
+  domain: 'https://www.notion.so/0xcc6347c/CJ-s-Blog-97d27de9a93a45d8a87c6985ecea15d5',
   author: 'Cheng JIANG',
 
   // open graph metadata (optional)
